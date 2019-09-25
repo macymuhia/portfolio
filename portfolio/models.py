8bb6270068341b642904b9f7165f1c6255fa237e
macymuhia <macymuhia@gmail.com>
2019-09-25 14:20:57 +0300

    Add model classes for profile, menu and project

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    profile_pic = models.ImageField(blank=True, default="")
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=255)
+    github_url = models.URLField()
+    linked_in_url = models.URLField()
+    twitter_url = models.URLField()
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=10)
+    description = models.TextField(max_length=255)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=255)
+    project_url = models.URLField()
+
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name="images")
+    image = models.ImageField()
+
+
+class ProjectTech(models.Model):
+    project = models.ForeignKey(Project, related_name="technologies")
+    name = models.CharField(max_length=20)
