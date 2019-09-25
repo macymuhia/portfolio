fef0297e069def14d153bd58473df7a83b12060b
macymuhia <macymuhia@gmail.com>
2019-09-25 14:22:13 +0300

    Register model classes

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 3
+
+
+class ProjectTechInline(admin.TabularInline):
+    model = ProjectTech
+    extra = 3
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectImageInline, ProjectTechInline]
+
+
+admin.site.register(Profile, Menu, Project, ProjectAdmin)
