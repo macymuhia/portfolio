1e1a3dadbba5ddb1c25b2f9f84b855b05dab3f60
macymuhia <macymuhia@gmail.com>
2019-09-25 17:04:41 +0300

    Add settings to allow image upload

--- macyportfolio/settings.py
+++ macyportfolio/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ]
         },
     }
@@ -116,4 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
