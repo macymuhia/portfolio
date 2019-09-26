1a5a08b52a4265ff29fad4f37b1cfb4cb3c748d3
macymuhia <macymuhia@gmail.com>
2019-09-26 09:16:06 +0300

    Add fontawesome5 to app and resize profile image

--- macyportfolio/settings.py
+++ macyportfolio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "portfolio",
     "bootstrap3",
+    "fontawesome_5",
 ]
 
 MIDDLEWARE = [
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from fontawesome_5.fields import IconField
 
 # Create your models here.
 class Profile(models.Model):
@@ -6,14 +7,17 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to="profile/", default="")
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=1000)
-    github_url = models.URLField()
-    linked_in_url = models.URLField()
-    twitter_url = models.URLField()
+    github = models.URLField()
+    linkedin = models.URLField()
+    twitter = models.URLField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     @classmethod
     def get_profile(cls):
         return cls.objects.get(pk=1)
+
+    def yearpublished(self):
+        return self.pub_date.strftime("%Y")
 
     def __str__(self):
         return self.name
